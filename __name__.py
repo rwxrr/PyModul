9b6071ad8603249565f32496e24674792449dd13
- <rwxrr@users.noreply.github.com>
2015-11-01 01:10:25 +0200

    Create __name__.py

---
+++ __name__.py
@@ -0,0 +1,10 @@
+# __name__ niteliği programınızın çalıştığı işletim sistemi bilgisini verir
+
+from os import name
+
+if name==posix:
+  print "Linus Torvalds'a mesajınız : "
+  
+if name==nt:
+  print "Bill Gates'e mesajınız"
+  
